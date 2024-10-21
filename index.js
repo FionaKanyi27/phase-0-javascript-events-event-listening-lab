2967a0c14fc0484e3bcf3f373629c46a21089464
FionaKanyi <fionakanyi0@gmail.com>
2024-10-21 17:53:43 +0300

    commit lab4

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+// Call the function to add the event listener when the DOM is loaded
+addingEventListener();
